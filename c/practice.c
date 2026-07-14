67dc33e3acdb518591e00d6a29d36314c7960913
Wangzm25 <wangzm2025011716@gmail.com>
2026-07-14 06:07:01 +0000

    修改两个整数求和程序

--- c/practice.c
+++ c/practice.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int a,b,sum;
+int a,c,sum;
 printf("The program adds two integers.\n");
 printf("The first number:");
 scanf("%d",&a);
 printf("The second number:");
-scanf("%d",&b);
-sum=a+b;
-printf("%d+%d=%d\n",a,b,sum);
+scanf("%d",&c);
+sum=a+c;
+printf("%d+%d=%d\n",a,c,sum);
 return 0;
 }
